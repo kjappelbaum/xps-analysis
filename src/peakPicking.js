15bfa1257e8d136fe91aefcf41f0ec6455ca21c7
Luc Patiny <luc@patiny.com>
2020-11-17 10:51:01 +0100

    fix: add peakPicking documentation

--- src/peakPicking.js
+++ src/peakPicking.js
@@ -1,5 +1,13 @@
 import { gsd } from 'ml-gsd';
 
+/**
+ * Allow
+ * @param {object} spectrum
+ * @param {object} [options={}]
+ * @param {object} [options.gsd={}] global spectra deconvolution options. Available options: http://mljs.github.io/global-spectral-deconvolution/#gsd
+ * @return {array} Array of peaks
+ */
+
 export function peakPicking(spectrum, options = {}) {
   const { gsd: gsdOptions = { realTopDetection: true } } = options;
   let data = {
