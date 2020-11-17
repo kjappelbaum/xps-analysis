76ac458e6c8d1dc87687bd0cac9c7923434b0782
Luc Patiny <luc@patiny.com>
2020-11-17 10:22:55 +0100

    chore: add test of vamas to jcamp conversion

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -4,15 +4,25 @@ import { join } from 'path';
 import { fromVamas, toJcamp } from '..';
 
 describe('fromVamas', () => {
-  const text = readFileSync(
-    join(__dirname, '../../testFiles/multiplex.vms'),
-    'utf8',
-  );
-
-  it('should return 42', () => {
+  it('VAMAS multiplex', () => {
+    const text = readFileSync(
+      join(__dirname, '../../testFiles/multiplex.vms'),
+      'utf8',
+    );
     let result = fromVamas(text);
 
     let jcamp = toJcamp(result);
-    console.log(jcamp);
+    expect(jcamp).toHaveLength(35450);
+  });
+
+  it('VAMAS assigned', () => {
+    const text = readFileSync(
+      join(__dirname, '../../testFiles/assigned.vms'),
+      'utf8',
+    );
+    let result = fromVamas(text);
+    let jcamp = toJcamp(result);
+    expect(jcamp.indexOf('CASA')).toBeGreaterThan(30000);
+    expect(jcamp).toHaveLength(579078);
   });
 });
