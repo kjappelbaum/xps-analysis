52db998f5667408002e50db13316db61189e9bd8
Luc Patiny <luc@patiny.com>
2021-04-27 12:01:37 +0200

    chore: improve getNormalizedMeta

--- src/from/__tests__/getNormalizedMeta.test.js
+++ src/from/__tests__/getNormalizedMeta.test.js
@@ -3,6 +3,7 @@ import { getNormalizedMeta } from '../getNormalizedMeta';
 import meta from './meta.json';
 
 test('appendCheminfoMeta', () => {
+  console.log(meta);
   const normalized = getNormalizedMeta(meta);
   console.log({ normalized });
 });
